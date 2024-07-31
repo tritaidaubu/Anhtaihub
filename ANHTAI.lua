0f0f0f0bdea3a6bd008b8dc350cffd50afbe9fc5
tritaidaubu <166419203+tritaidaubu@users.noreply.github.com>
2024-07-31 18:40:04 +0700

    Add files via upload

--- ANHTAI.lua
+++ ANHTAI.lua
@@ -84,105 +84,11 @@ local XSeaGui = XSeaLib:Start({
     ["Logo Info"] = "rbxassetid://17851124858",
     ["Logo Player"] = "rbxassetid://18735998414",
     ["Name Info"] = "Bap Mod Game Hub Info",
-    ["Name Player"] = "Min Gaming",
+    ["Name Player"] = "Bap Mod Gaming",
     ["Info Description"] = "Hihi",
     ["Tab Width"] = 135,
     ["Color"] = Color3.fromRGB(127.00000002980232, 146.00000649690628, 242.00000077486038),
     ["CloseCallBack"] = function() end
-})
-local MainTab = XSeaGui:MakeTab("Main")
-local Section = MainTab:Section({
-    ["Title"] = "Local Player",
-    ["Content"] = "Local Player Function"
-})
-local Seperator = Section:Seperator("Local Player")
-local Paragraph = Section:Paragraph({
-    ["Title"] = "Paragraph",
-    ["Content"] = ""
-})
-local Paragraph1 = Section:Paragraph({
-    ["Title"] = "Paragraph",
-    ["Content"] = "This is a Paragraph"
-})
-local Button = Section:Button({
-    ["Title"] = "Button",
-    ["Content"] = "",
-    ["Callback"] = function()
-        print("Button Clicked!")
-    end
-})
-local Button1 = Section:Button({
-    ["Title"] = "Button",
-    ["Content"] = "This is a Button",
-    ["Callback"] = function()
-        print("Button Clicked!")
-    end
-})
-local TextInput = Section:TextInput({
-    ["Title"] = "Text Input",
-    ["Content"] = "",
-    ["Place Holder Text"] = "Enter you text here...",
-    ["Clear Text On Focus"] = true,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local TextInput1 = Section:TextInput({
-    ["Title"] = "Text Input",
-    ["Content"] = "This is a Text Input",
-    ["Place Holder Text"] = "Enter you text here...",
-    ["Clear Text On Focus"] = true,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local Toggle = Section:Toggle({
-	["Title"]= "Toggle",
-	["Content"] = "",
-	["Default"] = true,
-	["Callback"] = function(Value) 
-		print(Value)
-	end
-})
-local Toggle1 = Section:Toggle({
-	["Title"]= "Toggle",
-	["Content"] = "This is a Toggle",
-	["Default"] = false,
-	["Callback"] = function(Value) 
-		print(Value)
-	end
-})
-local Slider = Section:Slider({
-    ["Title"] = "Slider",
-    ["Content"] = "",
-    ["Min"] = 0,
-    ["Max"] = 100,
-    ["Increment"] = 1,
-    ["Default"] = 30,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local Slider1 = Section:Slider({
-    ["Title"] = "Slider",
-    ["Content"] = "This is a Slider",
-    ["Min"] = 0,
-    ["Max"] = 100,
-    ["Increment"] = 1,
-    ["Default"] = 30,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local Dropdown = Section:Dropdown({
-    ["Title"] = "Dropdown",
-    ["Multi"] = true,
-    ["Options"] = {"Option 1", "Option 2"},
-    ["Default"] = {"Option 1"},
-    ["Place Holder Text"] = "Select Options",
-    ["Callback"] = function(Value)
-        print(Value)
-    end
 })
 local ProfileTab = XSeaGui:MakeTab("Profile")
 local Window = Fluent:CreateWindow({
